f6b1699f0ea4868d0c5f85e512f0d31885cd03fa
nathank33 <nathanbraunguitar@gmail.com>
2015-01-31 23:44:21 -0800

    Attributes have elements a variable to determine the correct direction for a benefit

--- src/com/projectkorra/ProjectKorraItems/attribute/Attribute.java
+++ src/com/projectkorra/ProjectKorraItems/attribute/Attribute.java
@@ -9,6 +9,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import com.projectkorra.ProjectKorra.Element;
 import com.projectkorra.ProjectKorraItems.Messages;
 import com.projectkorra.ProjectKorraItems.items.CustomItem;
 
@@ -17,12 +18,24 @@ public class Attribute
 	private String name;
 	private String desc;
 	private ArrayList<String> values;
+	private Element element;
 	private double duration, time;
+	private int benefit;
 	
-	public Attribute(String name, String desc) {
+	public Attribute(String name, String desc, Element element, int benefit) {
 		this.name = name;
 		this.desc = desc;
+		this.element = element;
+		this.benefit = benefit;
 		values = new ArrayList<String>();
+	}
+	
+	public Attribute(String name, String desc, Element element) {
+		this(name, desc, element, 1);
+	}
+	
+	public Attribute(String name, String desc) {
+		this(name, desc, null);
 	}
 	
 	public Attribute(String name) {
@@ -30,13 +43,17 @@ public class Attribute
 	}
 	
 	public Attribute() {
-		this("", "");
+		this("");
 	}
 	
 	public Attribute clone() {
 		Attribute attr = new Attribute();
 		attr.name = this.name;
 		attr.desc = this.desc;
+		attr.time = this.time;
+		attr.duration = this.duration;
+		attr.element = this.element;
+		attr.benefit = this.benefit;
 		ArrayList<String> newVals = new ArrayList<String>();
 		for(String str : this.values)
 			newVals.add(new String(str));
@@ -53,6 +70,22 @@ public class Attribute
 
 	public double getTime() {
 		return time;
+	}
+
+	public Element getElement() {
+		return element;
+	}
+
+	public void setElement(Element element) {
+		this.element = element;
+	}
+
+	public int getBenefit() {
+		return benefit;
+	}
+
+	public void setBenefit(int benefit) {
+		this.benefit = benefit;
 	}
 
 	public void setTime(double time) {
@@ -81,6 +114,11 @@ public class Attribute
 
 	public void setValues(ArrayList<String> values) {
 		this.values = values;
+	}
+	
+	public void setValues(double val) {
+		this.values = new ArrayList<String>();
+		values.add(val + "");
 	}
 	
 	public static Attribute getAttribute(String name) {
@@ -129,15 +167,14 @@ public class Attribute
 		 */
 		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
 		ConcurrentHashMap<String, Double> map = new ConcurrentHashMap<String, Double>();
+		ArrayList<Attribute> totalAttribs = new ArrayList<Attribute>();
 		
 		// Handle any potion style bending effects that the player might have
 		if(AttributeListener.currentBendingEffects.containsKey(player.getName())) {
 			ConcurrentHashMap<String, Attribute> effects = AttributeListener.currentBendingEffects.get(player.getName());
 			for(Attribute effect : effects.values()) {
 				if(System.currentTimeMillis() - effect.time < effect.duration) {
-					try {
-						map.put(effect.getName(), Double.parseDouble(effect.getValues().get(0).trim()));
-					} catch(Exception e) {}
+					totalAttribs.add(effect);
 				}
 			}
 		}
@@ -173,6 +210,7 @@ public class Attribute
 			if(!validCharges)
 				continue;
 			
+			// Handle the holdOnly/wearOnly stats
 			boolean wearHoldValid = true;
 			for(Attribute attr : citem.getAttributes()) {
 				if(attr.getBooleanValue("HoldOnly") && !istack.equals(player.getItemInHand())) {
@@ -186,23 +224,51 @@ public class Attribute
 			}
 			if(!wearHoldValid)
 				continue;
-			
-			for(Attribute attr : citem.getAttributes()) {
-				if(attr.getValues().size() != 1)
-					continue;
-				
-				double val = 0;
-				if(map.containsKey(attr.getName())) 
-					val = map.get(attr.getName());
-				try {
-					val += Double.parseDouble(attr.getValues().get(0));
-					map.put(attr.getName(), val);
-				} catch (NumberFormatException e) {
-					continue;
+			for(Attribute attr : citem.getAttributes())
+				totalAttribs.add(attr);
+		}
+		
+		// Handle the full element powerup stats 
+		for(int i = 0; i < totalAttribs.size(); i++) {
+			Attribute attr = totalAttribs.get(i);
+			String name = attr.getName();
+			if(name.equalsIgnoreCase("Air") || name.equalsIgnoreCase("Water") 
+					|| name.equalsIgnoreCase("Earth") || name.equalsIgnoreCase("Fire") 
+					|| name.equalsIgnoreCase("Chi")) {
+				Element elem = Element.getType(name);
+				if(elem != null) {
+					double val = 1;
+					try {
+						val = Double.parseDouble(attr.getValues().get(0));
+					}
+					catch (NumberFormatException e) {
+						continue;
+					}
+					for(Attribute listAttr : AttributeList.attributes) {
+						if(listAttr.element == elem) {
+							Attribute newAttr = listAttr.clone();
+							newAttr.setValues(val * newAttr.getBenefit());
+							totalAttribs.add(newAttr);
+						}
+					}
 				}
 			}
 		}
-		
+			
+		for(Attribute attr : totalAttribs) {
+			if(attr.getValues().size() != 1)
+				continue;
+			
+			double val = 0;
+			if(map.containsKey(attr.getName())) 
+				val = map.get(attr.getName());
+			try {
+				val += Double.parseDouble(attr.getValues().get(0));
+				map.put(attr.getName(), val);
+			} catch (NumberFormatException e) {
+				continue;
+			}
+		}
 		return map;
 	}
 
