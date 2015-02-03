14cc41ff271f103b63fd28933d36bf59e9a4a4e3
nathank33 <nathanbraunguitar@gmail.com>
2015-02-02 23:59:55 -0800

    Organized Item Methods

--- src/com/projectkorra/ProjectKorraItems/ItemUtils.java
+++ src/com/projectkorra/ProjectKorraItems/ItemUtils.java
@@ -7,6 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import com.projectkorra.ProjectKorraItems.attribute.Attribute;
+import com.projectkorra.ProjectKorraItems.attribute.AttributeList;
+import com.projectkorra.ProjectKorraItems.items.CustomItem;
+
 public class ItemUtils {
 
 	/**
@@ -14,12 +18,97 @@ public class ItemUtils {
 	 * @param player the player with the items
 	 * @return a list of items
 	 */
-	public static ArrayList<ItemStack> getPlayerItems(Player player) {
+	public static ArrayList<ItemStack> getPlayerEquipment(Player player) {
 		ArrayList<ItemStack> istacks = new ArrayList<ItemStack>();
 		for(ItemStack istack : player.getInventory().getArmorContents())
 			istacks.add(istack);
 		istacks.add(player.getItemInHand());
 		return istacks;
+	}
+	
+	/**
+	 * Returns a list of the players armor and in hand item, ONLY if
+	 * the item is usable for the specific player. If the item has the
+	 * "HoldOnly", "WearOnly", or "RequireElement" stat, then this
+	 * method may ignore some items.
+	 * @param player the player that has equipment
+	 * @return a list of the equipment
+	 */
+	public static ArrayList<ItemStack> getPlayerValidEquipment(Player player) {
+		ArrayList<ItemStack> equipment = getPlayerEquipment(player);
+		
+		for(int i = 0; i < equipment.size(); i++) {
+			ItemStack istack = equipment.get(i);
+			CustomItem citem = CustomItem.getCustomItem(istack);
+			if(citem == null)
+				continue;
+			
+			if(!hasValidCharges(istack)) {
+				equipment.remove(i);
+				i--;
+				continue;
+			}
+			else if(citem.getBooleanAttributeValue("HoldOnly") && !istack.equals(player.getItemInHand())) {
+				equipment.remove(i);
+				i--;
+				continue;
+			}
+			else if(citem.getBooleanAttributeValue("WearOnly") && istack.equals(player.getItemInHand())) {
+				equipment.remove(i);
+				i--;
+				continue;
+			}
+			
+			Attribute restrictElem = citem.getAttribute("RestrictElement");
+			if(restrictElem != null) {
+				boolean allowed = false;
+				for(String val : restrictElem.getValues()) {
+					try {
+						if(ElementUtils.hasElement(player, val)) {
+							allowed = true;
+							break;
+						}
+					}
+					catch(IllegalArgumentException e) {
+						Messages.logTimedMessage(e.getMessage());
+					}
+				}
+				
+				if(!allowed) {
+					equipment.remove(i);
+					i--;
+					continue;
+				}
+			}
+		}
+		return equipment;
+	}
+	
+	/**
+	 * Checks if an item has a usable amount of charges,
+	 * or doesn't require charges at all.
+	 * @param item the custom item
+	 * @return true if the item has a usable amount of charges, or no charges
+	 */
+	public static boolean hasValidCharges(ItemStack item) {
+		boolean validCharges = true;
+		try {
+			for(String line : item.getItemMeta().getLore()) {
+				if(line.startsWith(AttributeList.CHARGES_STR) || line.startsWith(AttributeList.CLICK_CHARGES_STR)
+						|| line.startsWith(AttributeList.SNEAK_CHARGES_STR)) {
+					String tmpStr = line.substring(line.indexOf(": ") + 1, line.length()).trim();
+					int value = Integer.parseInt(tmpStr);
+					if(value <= 0)
+						validCharges = false;
+					else {
+						validCharges = true;
+						break;
+					}
+				}
+			}
+		}
+		catch (Exception e) {}
+		return validCharges;
 	}
 	
 	/**
